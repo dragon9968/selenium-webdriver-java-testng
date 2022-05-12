70b330783ffbdb5e0008b18b6b18be13e56a0a01
long nguyen <dragon9968@gmail.com>
2022-05-12 00:53:16 -0700

    update

--- selenium/webDriver/Topic_06_WebBrowsers_WebElements.java
+++ selenium/webDriver/Topic_06_WebBrowsers_WebElements.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_06_Web_Browsers {
+public class Topic_06_WebBrowsers_WebElements {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	@BeforeClass
